fc6ff330a6eb78b2627a4603c05b1573d238882a
uamadasun <uchennaamadasun@gmail.com>
2023-01-05 14:14:17 -0800

    inject api call to the html with fake data from jsonplaceholder website

--- script.js
+++ script.js
@@ -3,3 +3,43 @@ const title = document.querySelector(".title");
 const btn = document.querySelector("#btn");
 
 //Making http request
+//1. get the endpoint of the server/api
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+// fetch(url)
+// .then((res)=>{
+//     res.json()
+//     .then((data) => {console.log(data);})
+// })
+// .catch((err)=>{console.log(err)});
+
+const fetchPosts = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();//axios would pass it for us automatically, would not need this line
+        console.log(data);
+
+        //display to DOM
+        const result = data.map((post) => {
+            return `
+            <div class="card">
+            <div class="card-title">
+                <h2>${post.title}</h2>
+            </div>
+
+            <div class="card-body">
+                <p>
+                    ${post.body}
+                </p>
+            </div>
+        </div>
+            `;
+        });
+        container.innerHTML = result.join("");
+    } catch (error) {
+        console.log(error);
+    }
+}
+fetchPosts();
+
+btn.addEventListener('click', fetchPosts);
